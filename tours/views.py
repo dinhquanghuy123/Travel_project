14e6c7bb7d071db362997d2d8fe7b85b83615e45
Dinh Huy <huyquangdinh1712@gmail.com>
2026-04-29 00:00:17 +0700

    complete tours

--- tours/views.py
+++ tours/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Tour, Review
 
 
 def home_view(request):
-    return HttpResponse("Home page")
+    tours = Tour.objects.all()
+    return render(request, 'tours/home.html', {'tours': tours})
 
 
 def tour_detail_view(request, id):
-    return HttpResponse(f"Tour {id}")
+    tour = get_object_or_404(Tour, id=id)
+    reviews = Review.objects.filter(tour=tour)
+
+    return render(request, 'tours/detail.html', {
+        'tour': tour,
+        'reviews': reviews
+    })
