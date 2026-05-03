0ad38b13d14de75629963f6741a73f317c6117e2
Dinh Huy <huyquangdinh1712@gmail.com>
2026-05-03 15:01:40 +0700

    DEBUG to False

--- config/settings.py
+++ config/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$yjt8-!$9h_m_kt16dmt-jnpr@mn9w^+lc=rhpt+@wd5xc7mb%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['dinhhuy1712.pythonanywhere.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dinhhuy1712.pythonanywhere.com']
 
 
 # Application definition
